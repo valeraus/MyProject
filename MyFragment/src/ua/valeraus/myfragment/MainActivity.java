e3f97e13b83cc275e8c1290a272751f52e035209
valeraus <vlaus0@gmail.com>
2015-01-27 10:57:55 +0200

    Install Upload

---
+++ MyFragment/src/ua/valeraus/myfragment/MainActivity.java
@@ -0,0 +1,83 @@
+package ua.valeraus.myfragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity {
+
+	Intent intent = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	// у атрибута пункта меню Settings установлено значение
+	// android:onClick="onSettingsMenuClick"
+	// public void onSettingsMenuClick(MenuItem item) {
+	//
+	// Toast.makeText(getApplicationContext(), "Setting", Toast.LENGTH_SHORT)
+	// .show();
+	// }
+	//
+	// public void onArchiveMenuClick(MenuItem item) {
+	//
+	// Toast.makeText(getApplicationContext(), "Arhive", Toast.LENGTH_SHORT)
+	// .show();
+	// }
+	//
+	// public void onTrashMenuClick(MenuItem item) {
+	//
+	// Toast.makeText(getApplicationContext(), "Trash", Toast.LENGTH_SHORT)
+	// .show();
+	// }
+	//
+	// public void onNotesMenuClick(MenuItem item) {
+	//
+	// Toast.makeText(getApplicationContext(), "Notes", Toast.LENGTH_SHORT)
+	// .show();
+	//
+	// intent = new Intent(this, CreateNoteActivity.class);
+	// intent.putExtra("isNewNote", true);
+	// startActivity(intent);
+	// }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		// Операции для выбранного пункта меню
+		switch (id) {
+		case R.id.action_notes:
+			// infoTextView.setText("1");
+			Toast.makeText(getApplicationContext(), "Notes", Toast.LENGTH_SHORT)
+					.show();
+			return true;
+		case R.id.action_trash:
+			// infoTextView.setText("2");
+			return true;
+		case R.id.action_archive:
+			// infoTextView.setText("3");
+			return true;
+		case R.id.action_setting:
+			// infoTextView.setText("4");
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
